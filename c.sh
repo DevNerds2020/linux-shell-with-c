6ac9ab969757da0252f4645662bad986b29876f8
amirreza <notname1957@gmail.com>
2022-12-29 21:06:47 +0330

    optimized c linux command

--- c.sh
+++ c.sh
@@ -1,3 +1,6 @@
-#delete space \t \n and then show the result
-sed -i 's/[ \t]*$//' $1 
-cat $1
+#delete space \t and then show the result
+# sed -i 's/[ \t]*$//' $1 
+# cat $1
+result=$(cat $1 | tr -d " \t\n\r")
+echo $result
+echo $result > $1
